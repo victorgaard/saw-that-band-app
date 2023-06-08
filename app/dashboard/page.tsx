33adb88b3b8dd87112a1c6ed3aa54df7f2017778
Victor F. Santos <victor.gaard@gmail.com>
2023-06-08 07:36:35 +0300

    chore: prettier and husky

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import { AuthContext } from "@/auth/AuthContext";
-import BandsListRenderItem from "@/components/BandsListRender";
-import SearchBar from "@/components/SearchBar";
-import { ToastContext } from "@/components/Toast/ToastContext";
-import { Database } from "@/types/supabase";
-import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useContext, useEffect, useRef, useState } from "react";
-import AutoSizer from "react-virtualized-auto-sizer";
-import { FixedSizeList as List, ListChildComponentProps } from "react-window";
+import { AuthContext } from '@/auth/AuthContext';
+import BandsListRenderItem from '@/components/BandsListRender';
+import SearchBar from '@/components/SearchBar';
+import { ToastContext } from '@/components/Toast/ToastContext';
+import { Database } from '@/types/supabase';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+import { useContext, useEffect, useRef, useState } from 'react';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { FixedSizeList as List, ListChildComponentProps } from 'react-window';
 
 function Dashboard() {
   const { supabase, user } = useContext(AuthContext);
   const { toast } = useContext(ToastContext);
 
   const [bands, setBands] =
-    useState<Database["public"]["Tables"]["Bands"]["Row"][]>();
-  const [query, setQuery] = useState("");
+    useState<Database['public']['Tables']['Bands']['Row'][]>();
+  const [query, setQuery] = useState('');
 
   const listRef = useRef<List>(null);
 
@@ -29,17 +29,17 @@ function Dashboard() {
       if (!user) return;
 
       const { data, error } = await supabase
-        .from("Bands")
+        .from('Bands')
         .select()
-        .eq("user_id", user.id)
-        .order("band", { ascending: true });
+        .eq('user_id', user.id)
+        .order('band', { ascending: true });
 
       if (error)
         return toast({
-          type: "error",
-          title: "Bands could not be loaded",
+          type: 'error',
+          title: 'Bands could not be loaded',
           message:
-            "There was an error loading your bands. Please try again later.",
+            'There was an error loading your bands. Please try again later.'
         });
 
       setBands(data);
@@ -51,13 +51,13 @@ function Dashboard() {
 
   const treatedQuery = query.trim().toLowerCase();
   const filteredBands = bands?.filter(
-    (band) =>
+    band =>
       band.band.toLowerCase().includes(treatedQuery) ||
-      band.genre.some((genre) => genre.toLowerCase().includes(treatedQuery)) ||
-      band.concerts.some((concert) =>
+      band.genre.some(genre => genre.toLowerCase().includes(treatedQuery)) ||
+      band.concerts.some(concert =>
         concert.location.toLowerCase().includes(treatedQuery)
       ) ||
-      band.concerts.some((concert) =>
+      band.concerts.some(concert =>
         concert.date.toLowerCase().includes(treatedQuery)
       )
   );
@@ -71,7 +71,7 @@ function Dashboard() {
           query={query}
           setQuery={setQuery}
           bandsCount={filteredBands.length}
-          handleChange={(e) => setQuery(e.target.value)}
+          handleChange={e => setQuery(e.target.value)}
         />
         {filteredBands.length === 0 && (
           <div className="mt-20 flex flex-col items-center justify-center gap-8 px-8 pt-8 sm:mt-48">
@@ -83,7 +83,7 @@ function Dashboard() {
             </div>
             <button
               onClick={() => {
-                setQuery("");
+                setQuery('');
               }}
               type="button"
               className="flex items-center gap-4 rounded bg-zinc-900 p-4 text-zinc-400 hover:bg-zinc-700/50 hover:text-white sm:hidden"
@@ -118,7 +118,9 @@ function Dashboard() {
           </AutoSizer>
         </div>
       </div>
-      <div className="bg-zinc-900 py-6 p-12">Select a band or add a new band</div>
+      <div className="bg-zinc-900 p-12 py-6">
+        Select a band or add a new band
+      </div>
     </div>
   );
 }
