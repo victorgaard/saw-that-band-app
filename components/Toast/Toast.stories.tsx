e50c7a333f80ed1790d6a0606769cbf7e710dcc9
Victor F. Santos <victor.gaard@gmail.com>
2023-06-08 08:16:14 +0300

    chore: cleaning up files

--- components/Toast/Toast.stories.tsx
+++ components/Toast/Toast.stories.tsx
@@ -2,17 +2,33 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Toast from './Toast';
 
 const meta: Meta<typeof Toast> = {
-  component: Toast
+  title: 'Components/Toast',
+  component: Toast,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: 'inline-radio'
+    }
+  }
 };
 
 export default meta;
 type Story = StoryObj<typeof Toast>;
 
-export const Primary: Story = {
+export const Success: Story = {
   args: {
     type: 'success',
     title: 'Band added',
     message: 'The band Alcest was successfully added to your bands inventory',
+    dismiss: () => {}
+  }
+};
+export const Error: Story = {
+  args: {
+    type: 'error',
+    title: 'Band not added',
+    message:
+      'The band Alcest could not be added to your bands inventory. Please try again later.',
     dismiss: () => {}
   }
 };
