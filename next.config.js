60e4fa060b74b887ddd246920b4ff06aabe195de
Victor F. Santos <victor.gaard@gmail.com>
2023-08-18 20:40:24 +0200

    chore: remove image optimization

--- next.config.js
+++ next.config.js
@@ -1,25 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.scdn.co",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.countryflagicons.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "guerfzlhzjrpooirzvlf.supabase.co",
-        pathname: "/**",
-      },
-    ],
-  },
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
